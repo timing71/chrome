f38434b577ee57709c591b9605856d97cd1992a0
James Muscat <jamesremuscat@gmail.com>
2021-09-18 13:31:40 +0100

    Tie in services to the background script.

--- src/background.js
+++ src/background.js
@@ -1,6 +1,7 @@
 /* global chrome */
 
 import { setupPageRules } from "./pageRules";
+import { fetchService, startService, terminateService, updateServiceState } from "./services";
 
 const URL_ROOT = process.env.NODE_ENV === 'production' ? 'https://beta.timing71.org' : 'http://localhost:3000';
 
@@ -13,7 +14,9 @@ chrome.runtime.onInstalled.addListener(
 
 chrome.action.onClicked.addListener(
   (currentTab) => {
-    chrome.tabs.create({ url: `${URL_ROOT}/start?source=${currentTab.url}` });
+    chrome.tabs.create({ url: `${URL_ROOT}/start?source=${currentTab.url}` }).then(
+      tab => chrome.tabs.update(tab.id, { autoDiscardable: false })
+    );
   },
 );
 
@@ -21,10 +24,54 @@ chrome.runtime.onConnectExternal.addListener(
   (port) => {
     port.onMessage.addListener(
       (message, otherPort) => {
-        if (message.type === 'HANDSHAKE') {
-          otherPort.postMessage({
-            type: 'HANDSHAKE_RETURN'
-          });
+
+        switch(message.type) {
+          case 'HANDSHAKE':
+            otherPort.postMessage({
+              type: 'HANDSHAKE_RETURN'
+            });
+            break;
+
+          case 'START_SERVICE':
+            startService(message.uuid, message.source).then(
+              otherPort.postMessage({
+                type: 'START_SERVICE_RETURN',
+                originalMessage: message
+              })
+            );
+            break;
+
+          case 'TERMINATE_SERVICE':
+            terminateService(message.uuid);
+            break;
+
+          case 'FETCH_SERVICE':
+            fetchService(message.uuid).then(
+              ss => otherPort.postMessage({
+                type: 'FETCH_SERVICE_RETURN',
+                ...ss
+              })
+            );
+            break;
+
+          case 'STORE_SERVICE_STATE':
+            updateServiceState(message.uuid, message.state);
+            break;
+
+          case 'FETCH':
+            fetch(message.url).then(
+              response => response.text().then(
+                text => otherPort.postMessage({
+                  type: 'FETCH_RETURN',
+                  data: text,
+                  originalMessage: message
+                })
+              )
+            );
+            break;
+
+          default:
+
         }
       }
     );
---
+++ src/services.js
@@ -0,0 +1,44 @@
+import Dexie from "dexie";
+
+const db = new Dexie('timing71_services');
+
+const DEFAULT_STATE = {
+  cars: [],
+  session: {},
+  messages: []
+};
+
+db.version(1).stores({
+  services: 'uuid',
+  service_states: 'uuid'
+});
+
+export const startService = async (uuid, source) => {
+  await db.services.put({
+    uuid,
+    source,
+    startTime: Date.now()
+  });
+  await db.service_states.put({
+    uuid,
+    state: { ...DEFAULT_STATE }
+  });
+};
+
+export const terminateService = async (uuid) => {
+  await db.services.delete(uuid);
+  await db.service_states.delete(uuid);
+};
+
+export const fetchService = async (uuid) => {
+  const service = await db.services.get(uuid);
+  const state = await db.service_states.get(uuid);
+  return {
+    service,
+    state: state.state
+  };
+};
+
+export const updateServiceState = async (uuid, state) => {
+  await db.service_states.put({ uuid, state });
+};
