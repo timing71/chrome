a8d977216d649d3a62703fa742e60579d4c78090
James Muscat <jamesremuscat@gmail.com>
2021-09-20 12:46:01 +0100

    Better handle port timeouts.

--- src/background.js
+++ src/background.js
@@ -4,6 +4,8 @@ import { fetchService, startService, terminateService, updateServiceState } from
 
 const URL_ROOT = process.env.NODE_ENV === 'production' ? 'https://beta.timing71.org' : 'http://localhost:3000';
 
+const openPorts = [];
+
 chrome.runtime.onInstalled.addListener(
   () => {
     chrome.action.disable();
@@ -19,72 +21,86 @@ chrome.action.onClicked.addListener(
   },
 );
 
+const handlePortMessage = (message, otherPort) => {
+  switch(message.type) {
+    case 'HANDSHAKE':
+      otherPort.postMessage({
+        type: 'HANDSHAKE_RETURN'
+      });
+      break;
+
+    case 'START_SERVICE':
+      startService(message.uuid, message.source).then(
+        otherPort.postMessage({
+          type: 'START_SERVICE_RETURN',
+          originalMessage: message
+        })
+      );
+      break;
+
+    case 'TERMINATE_SERVICE':
+      terminateService(message.uuid);
+      break;
+
+    case 'FETCH_SERVICE':
+      fetchService(message.uuid).then(
+        ss => otherPort.postMessage({
+          type: 'FETCH_SERVICE_RETURN',
+          ...ss
+        })
+      );
+      break;
+
+    case 'STORE_SERVICE_STATE':
+      updateServiceState(message.uuid, message.state);
+      break;
+
+    case 'FETCH':
+      fetch(message.url).then(
+        response => response.text().then(
+          text => otherPort.postMessage({
+            type: 'FETCH_RETURN',
+            data: text,
+            originalMessage: message
+          })
+        )
+      );
+      break;
+
+    case 'WEBSOCKET':
+      const ws = new WebSocket(`${message.url}`);
+
+      ws.onmessage = (msg) => {
+        openPorts.forEach(
+          openPort =>
+            openPort.postMessage({
+              type: 'WEBSOCKET_MESSAGE',
+              data: msg.data,
+              tag: message.tag
+            }
+          )
+        );
+      };
+
+      break;
+
+    default:
+  }
+};
+
 chrome.runtime.onConnectExternal.addListener(
   (port) => {
-    port.onMessage.addListener(
-      (message, otherPort) => {
+    port.onMessage.addListener(handlePortMessage);
 
-        switch(message.type) {
-          case 'HANDSHAKE':
-            otherPort.postMessage({
-              type: 'HANDSHAKE_RETURN'
-            });
-            break;
-
-          case 'START_SERVICE':
-            startService(message.uuid, message.source).then(
-              otherPort.postMessage({
-                type: 'START_SERVICE_RETURN',
-                originalMessage: message
-              })
-            );
-            break;
-
-          case 'TERMINATE_SERVICE':
-            terminateService(message.uuid);
-            break;
-
-          case 'FETCH_SERVICE':
-            fetchService(message.uuid).then(
-              ss => otherPort.postMessage({
-                type: 'FETCH_SERVICE_RETURN',
-                ...ss
-              })
-            );
-            break;
-
-          case 'STORE_SERVICE_STATE':
-            updateServiceState(message.uuid, message.state);
-            break;
-
-          case 'FETCH':
-            fetch(message.url).then(
-              response => response.text().then(
-                text => otherPort.postMessage({
-                  type: 'FETCH_RETURN',
-                  data: text,
-                  originalMessage: message
-                })
-              )
-            );
-            break;
-
-          case 'WEBSOCKET':
-            const ws = new WebSocket(`${message.url}`);
-            ws.onmessage = (msg) => {
-              otherPort.postMessage({
-                type: 'WEBSOCKET_MESSAGE',
-                data: msg.data,
-                tag: message.tag
-              });
-            };
-
-            break;
-
-          default:
-
+    port.onDisconnect.addListener(
+      (p) => {
+        const idx = openPorts.indexOf(p);
+        if (idx >= 0) {
+          openPorts.splice(idx, 1);
         }
       }
     );
+
+    openPorts.push(port);
   }
 );
