4afcb3bf9463522fc4f0a7e0f9b51e7342f5cf2d
James Muscat <jamesremuscat@gmail.com>
2022-05-23 13:16:19 +0100

    URL-encode `source` paramter to preserve other GET params.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 const URL_ROOT = process.env.NODE_ENV === 'production' ? 'https://beta.timing71.org' : 'http://localhost:3000';
 
-export const createStartURL = (source) => (`${URL_ROOT}/start?source=${source}`);
+export const createStartURL = (source) => (`${URL_ROOT}/start?source=${encodeURIComponent(source)}`);
 
 export const createPageURL = (page) => (`${URL_ROOT}/${page}`);
 
