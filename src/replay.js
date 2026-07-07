071140d11916c275883dd93ec966f17c40a798d4
James Muscat <jamesremuscat@gmail.com>
2026-07-07 12:15:23 +0100

    Paginate replay generation to (hopefully) minimise memory hogging.

--- src/replay.js
+++ src/replay.js
@@ -3,6 +3,8 @@ import { getAllServiceStates, getAnalysisAtIndex } from "./services";
 
 const zip = require("@zip.js/zip.js");
 
+const STATES_PAGE_SIZE = 1000;
+
 export const generateReplay = async (serviceUUID, sessionIndex=0, onProgress) => {
   let states = await getAllServiceStates(serviceUUID, sessionIndex);
   const stateCount = await states.count();
@@ -23,53 +25,56 @@ export const generateReplay = async (serviceUUID, sessionIndex=0, onProgress) =>
     new zip.TextReader(JSON.stringify(manifest))
   );
 
-  // Note: states is implicitly sorted per the `where` clause used in the Dexie query.
-  // https://github.com/dexie/Dexie.js/issues/300 would be useful here... but
-  // we should "only" be dealing with a few hundred MB of data
-  states = await getAllServiceStates(serviceUUID, sessionIndex);
-  const statesArray = await states.toArray();
+  for (let offset=0; offset <= stateCount; offset += STATES_PAGE_SIZE) {
+    // Note: states is implicitly sorted per the `where` clause used in the Dexie query.
+    // https://github.com/dexie/Dexie.js/issues/300 would be useful here... but
+    // we should "only" be dealing with a few hundred MB of data
+    states = await getAllServiceStates(serviceUUID, sessionIndex);
+    const statesArray = await states.offset(offset).limit(STATES_PAGE_SIZE).toArray();
 
-  let idx = 0;
+    let idx = offset;
 
-  let prevState = null;
-  let prevTime = null;
-  let stateCounter = 0;
-  let prevFrameFailed = false;
+    let prevState = null;
+    let prevTime = null;
+    let stateCounter = 0;
+    let prevFrameFailed = false;
 
-  for (const { state, timestamp } of statesArray) {
-    delete state.manifest;
+    for (const { state, timestamp } of statesArray) {
+      delete state.manifest;
 
-    const timePart = `${Math.floor(timestamp / 1000)}`.padStart(11, '0');
-    // If the new time part is identical to the previous time part (i.e. we had
-    // two frames within a second) then the second should be written as a kframe
-    // to avoid jumping states and having iframes not apply cleanly.
-    const shouldBeIframe = prevFrameFailed ||
-      (!!prevState &&
-        (stateCounter++ % 10) !== 0 &&
-        timePart !== prevTime
-      );
+      const timePart = `${Math.floor(timestamp / 1000)}`.padStart(11, '0');
+      // If the new time part is identical to the previous time part (i.e. we had
+      // two frames within a second) then the second should be written as a kframe
+      // to avoid jumping states and having iframes not apply cleanly.
+      const shouldBeIframe = prevFrameFailed ||
+        (!!prevState &&
+          (stateCounter++ % 10) !== 0 &&
+          timePart !== prevTime
+        );
 
-    const filePart = `${shouldBeIframe ? 'i' : ''}.json`;
-    const filename = `${timePart}${filePart}`;
+      const filePart = `${shouldBeIframe ? 'i' : ''}.json`;
+      const filename = `${timePart}${filePart}`;
 
-    const writableState = shouldBeIframe ? createIframe({ ...prevState }, state) : state;
-    try {
-      await writer.add(
-        filename,
-        new zip.TextReader(JSON.stringify(writableState))
-      );
-      prevFrameFailed = false;
+      const writableState = shouldBeIframe ? createIframe({ ...prevState }, state) : state;
+      try {
+        await writer.add(
+          filename,
+          new zip.TextReader(JSON.stringify(writableState))
+        );
+        prevFrameFailed = false;
+      }
+      catch {
+        // Most likely a "file already exists" error from zip.js
+        // So long as we make the subsequent frame a keyframe, it's probably fine
+        // to ignore this
+        prevFrameFailed = true;
+      }
+      onProgress({ item: idx++, total: stateCount, percent: Math.floor(100 * idx / stateCount) });
+      prevState = { ...state };
+      prevTime = timePart;
     }
-    catch {
-      // Most likely a "file already exists" error from zip.js
-      // So long as we make the subsequent frame a keyframe, it's probably fine
-      // to ignore this
-      prevFrameFailed = true;
-    }
-    onProgress({ item: idx++, total: stateCount, percent: Math.floor(100 * idx / stateCount) });
-    prevState = { ...state };
-    prevTime = timePart;
   }
+
 
   await writer.close();
 
